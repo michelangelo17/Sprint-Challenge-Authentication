0901774bcbb506e16c9b2bf7f0a1c9877b2370f5
michelangelo17 <michelangelo.markus@gmail.com>
2020-01-31 11:52:00 -0800

    added path for serving react app

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const helmet = require('helmet')
 
@@ -8,11 +9,17 @@ const jokesRouter = require('../jokes/jokes-router.js')
 
 const server = express()
 
+server.use(express.static(path.join('../client/build')))
+
 server.use(helmet())
 server.use(cors())
 server.use(express.json())
 
 server.use('/api/auth', authRouter)
 server.use('/api/jokes', authenticate, jokesRouter)
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join('../client/build/index.html'))
+})
 
 module.exports = server
