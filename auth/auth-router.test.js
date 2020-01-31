fe439b5c5239666a95e2cf4d2727758918220016
michelangelo17 <michelangelo.markus@gmail.com>
2020-01-31 09:28:25 -0800

    missing body tests added MVP complete

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -6,7 +6,7 @@ describe('authRouter', () => {
   beforeAll(async () => await db('users').truncate())
 
   describe('/register', () => {
-    describe('register with correct request body', () => {
+    describe('with correct request body', () => {
       test('returns a status of 201, a message, and a token', async () => {
         const res = await request(server)
           .post('/api/auth/register')
@@ -21,9 +21,26 @@ describe('authRouter', () => {
         expect(JSON.parse(res.text).token).toBeTruthy()
       })
     })
+
+    describe('with missing request body', () => {
+      test('returns a status of 500, a message, error, and no token', async () => {
+        const res = await request(server).post('/api/auth/register')
+
+        expect(res.status).toBe(500)
+
+        expect(JSON.parse(res.text).message).toBe('Uh Oh! 500 Error!')
+
+        expect(JSON.parse(res.text).error).toBe(
+          'Must send both a username and a password'
+        )
+
+        expect(JSON.parse(res.text).token).toBeFalsy()
+      })
+    })
   })
+
   describe('/login', () => {
-    describe('register with correct request body', () => {
+    describe('with correct request body', () => {
       test('returns a status of 201, a message, and a token', async () => {
         const res = await request(server)
           .post('/api/auth/login')
@@ -34,6 +51,22 @@ describe('authRouter', () => {
         expect(JSON.parse(res.text).message).toBe('atestuser logged in!')
 
         expect(JSON.parse(res.text).token).toBeTruthy()
+      })
+    })
+
+    describe('with missing request body', () => {
+      test('returns a status of 500, a message, error, and no token', async () => {
+        const res = await request(server).post('/api/auth/login')
+
+        expect(res.status).toBe(500)
+
+        expect(JSON.parse(res.text).message).toBe('Uh Oh! 500 Error!')
+
+        expect(JSON.parse(res.text).error).toBe(
+          'Must send both a username and a password'
+        )
+
+        expect(JSON.parse(res.text).token).toBeFalsy()
       })
     })
   })
