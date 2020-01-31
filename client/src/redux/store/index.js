65ac03c62b5f9bbe8e29ac1d89dc3a447a3c0533
michelangelo17 <michelangelo.markus@gmail.com>
2020-01-31 12:39:44 -0800

    disabled redux dev tools in production

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -4,6 +4,7 @@ import jokesReducer from '../slices/jokesSlice'
 
 const store = configureStore({
   reducer: { auth: authReducer, jokes: jokesReducer },
+  devTools: process.env.NODE_ENV !== 'production',
 })
 
 export default store
